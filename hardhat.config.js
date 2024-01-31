9f74ab04c3202a4058059f81120b5bc8713b2f88
Shashwat Singh <shashwat123student@gmail.com>
2024-01-31 22:42:57 +0530

    Added an task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config()
+require("./tasks/block-number")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-
 
 module.exports = {
   defaultNetwork: "hardhat",
